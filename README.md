8c07e8919710b276dee6bca73caf08902a2d8782
Gavela <gavelamm@gmail.com>
2026-06-24 17:10:59 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Math_250_Projects
+
+This is a sandbox repo for the summer 2026 math 250 class/
--- class notes 6.24 .R
+++ class notes 6.24 .R
@@ -5,5 +5,17 @@ library(tidyverse)
 
 create_github_token() # a token fromm github
 
-
+# connect the token
 gitcreds::gitcreds_set()
+
+use_github()
+
+## remote a repo that can be found online
+## origin is the main online repo
+## bio conductor
+
+# adding a read me
+
+use_readme_md()
+
+# commit and push
